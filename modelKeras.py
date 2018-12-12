5d852a2013987cc7934b4367d7a4b35faf7ba02b
FLYANALYSIS\labuser <matthew.creamer@yale.edu>
2018-12-12 17:23:26 -0500

    Updated HRC model to have arbitrary filters and added summation across space after combining filters

--- modelKeras.py
+++ modelKeras.py
@@ -16,40 +16,45 @@ def ln_model(input_shape=(11, 9, 1), filter_shape=(21, 9), num_filter=2):
     # Define the input as a tensor with shape input_shape
     X_input = Input(input_shape)
 
-    pad = int((filter_shape[0] - 1) / 2)
+    pad_x = int((filter_shape[1] - 1) / 2)
+    pad_t = int((filter_shape[0] - 1) / 2)
 
-    inputConv = Conv2D(num_filter, filter_shape, strides=(1, 1), name='conv1', kernel_initializer=glorot_uniform(seed=None))
+    input_conv = Conv2D(num_filter, filter_shape, strides=(1, 1), name='conv1', kernel_initializer=glorot_uniform(seed=None))
 
     # left arm
-    conv1 = inputConv(X_input)
+    conv1 = input_conv(X_input)
     conv1_a = Activation('relu')(conv1)
 
     # right arm
     # make reversal layers
     reverseLayer2 = Lambda(lambda x: K.reverse(x, axes=2))
 
+    # make sum layer
+    sum_layer = Lambda(lambda lam: K.sum(lam, axis=2, keepdims=True))
+
     reversedInput = reverseLayer2(X_input)
-    conv2 = reverseLayer2(inputConv(reversedInput))
+    conv2 = reverseLayer2(input_conv(reversedInput))
+
     conv2_a = Activation('relu')(conv2)
 
     subtractedLayer = subtract([conv1_a, conv2_a])
-    #subtractedLayer = concatenate([conv1_a, conv2_a], axis=3)
-    subtractedLayer_with_lin = concatenate([subtractedLayer, conv1, conv2], axis=3)
-    #subtractedLayer_with_lin = conv1_a
-    #subtractedLayer_with_lin = subtractedLayer
+    # subtractedLayer = concatenate([conv1_a, conv2_a], axis=3)
 
-    size_x_conv1 = int(subtractedLayer_with_lin.shape[2])
-    X = Conv2D(1, (1, size_x_conv1), strides=(1, 1), name='conv2', kernel_initializer=glorot_uniform(seed=None))(subtractedLayer_with_lin)
+    # conv_x_size = int(x_layer2.shape[2])
+    conv_x_size = 1
+    combine_filters = Conv2D(1, (1, conv_x_size), strides=(1, 1), name='conv2', kernel_initializer=glorot_uniform(seed=None))(subtractedLayer)
 
+    averaged_space = sum_layer(combine_filters)
+    
     # Create model
-    model = Model(inputs=X_input, outputs=X, name='SimpleMotion')
+    model = Model(inputs=X_input, outputs=averaged_space, name='SimpleMotion')
 
-    return model, pad, learning_rate, batch_size
+    return model, pad_x, pad_t, learning_rate, batch_size
 
 
-def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_filter=(4, 4)):
+def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_filter=(4, 1)):
     learning_rate = 0.001*1
-    batch_size = np.power(2, 8)
+    batch_size = np.power(2, 6)
     # ln model of the structure
     # conv, relu, conv, relu, subtract off mirror symmetric subunits, linear combination
 
@@ -57,7 +62,8 @@ def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_f
     x_input = Input(input_shape)
 
     # output the amount that this model will reduce the time variable by
-    pad = int((filter_shape[0][0] - 1) / 2 + (filter_shape[1][0] - 1) / 2)
+    pad_x = int((filter_shape[0][1] - 1) / 2 + (filter_shape[1][1] - 1) / 2)
+    pad_t = int((filter_shape[0][0] - 1) / 2 + (filter_shape[1][0] - 1) / 2)
 
     # define the initial convolution without inputs so we can reuse it. We want to train the same weights for the mirror
     # symmetric units
@@ -65,8 +71,12 @@ def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_f
                         kernel_initializer=glorot_uniform(seed=None))
     conv_2 = Conv2D(num_filter[1], filter_shape[1], strides=(1, 1), name='conv2',
                         kernel_initializer=glorot_uniform(seed=None))
+
     # make reversal layers to flip convolutions
     reverse_layer = Lambda(lambda lam: K.reverse(lam, axes=2))
+
+    # make sum layer
+    sum_layer = Lambda(lambda lam: K.sum(lam, axis=2, keepdims=True))
 
     # START actually computing x
     # non reversed path
@@ -78,8 +88,8 @@ def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_f
     x_1_reverse = Activation('relu')(x_1_reverse)
 
     # concatinate 2
-    x_layer1 = concatenate([x_1, x_1_reverse], axis=3)
-    #x_layer1 = x_1
+    x_layer1 = subtract([x_1, x_1_reverse])
+    # x_layer1 = concatenate([x_1, x_1_reverse], axis=3)
 
     # non reversed 2nd convolution
     x_2 = conv_2(x_layer1)
@@ -91,57 +101,71 @@ def ln_model_deep(input_shape=(11, 9, 1), filter_shape=((21, 5), (21, 5)), num_f
 
     # concatinate 2
     x_layer2 = concatenate([x_2, x_2_reverse], axis=3)
+    #x_layer2 = subtract([x_2, x_2_reverse])
     #x_layer2 = x_2
 
-    size_x_layer2 = int(x_layer2.shape[2])
-    x_out = Conv2D(1, (1, size_x_layer2), strides=(1, 1), name='x_out',
+    #conv_x_size = int(x_layer2.shape[2])
+    conv_x_size = 1
+    combine_filters = Conv2D(1, (1, conv_x_size), strides=(1, 1), name='x_out',
                kernel_initializer=glorot_uniform(seed=None))(x_layer2)
 
+    model_sum = sum_layer(combine_filters)
+
     # Create model
-    model = Model(inputs=x_input, outputs=x_out, name='DeepLn')
+    model = Model(inputs=x_input, outputs=model_sum, name='DeepLn')
 
-    return model, pad, learning_rate, batch_size
+    return model, pad_x, pad_t, learning_rate, batch_size
 
 
-def hrc_model(input_shape=(11, 9, 1), filter_shape=(21, 2), num_filter=2):
+def hrc_model(input_shape=(11, 9, 1), filter_shape=(21, 2), num_hrc=1):
     # set the learning rate that works for this model
     learning_rate = 0.001 * 1
+    batch_size = np.power(2, 6)
 
-    # output the amount that this model will reduce the time variable by
-    pad = int((filter_shape[0] - 1) / 2)+int((filter_shape[0] - 1) / 2)
-
+    # output the amount that this model will reduce the space and time variable by
+    pad_x = int((filter_shape[1] - 1) / 2)
+    pad_t = int((filter_shape[0] - 1) / 2)
 
     # Define the input as a tensor with shape input_shape
-    X_input = Input(input_shape)
+    model_input = Input(input_shape)
 
-
-
-    inputConv = Conv2D(num_filter, filter_shape, strides=(1, 1), name='conv1',
+    left_in = Conv2D(num_hrc, filter_shape, strides=(1, 1), name='conv1',
                        kernel_initializer=glorot_uniform(seed=None))
+    right_in = Conv2D(num_hrc, filter_shape, strides=(1, 1), name='conv2',
+                      kernel_initializer=glorot_uniform(seed=None))
 
-    leftArm_conv = inputConv(X_input)
-    leftArm_1_conv = Lambda(lambda x: x[:, :, :, 0, None])(leftArm_conv)
-    leftArm_2_conv = Lambda(lambda x: x[:, :, :, 1, None])(leftArm_conv)
+    # make reversal layers to flip convolutions
+    reverse_layer = Lambda(lambda lam: K.reverse(lam, axes=2))
 
-    multiplyLayer_leftArm = multiply([leftArm_1_conv, leftArm_2_conv])
+    # make sum layer
+    sum_layer = Lambda(lambda lam: K.sum(lam, axis=2, keepdims=True))
 
-    reverseLayer2 = Lambda(lambda x: K.reverse(x, axes=2))
-    rightArm_conv = inputConv(reverseLayer2(X_input))
-    rightArm_1_conv = Lambda(lambda x: K.reverse(x[:, :, :, 0, None], axes=2))(rightArm_conv)
-    rightArm_2_conv = Lambda(lambda x: K.reverse(x[:, :, :, 1, None], axes=2))(rightArm_conv)
+    # calculate unit1 inputs
+    unit1_left = left_in(model_input)
+    unit1_right = right_in(model_input)
 
-    multiplyLayer_rightArm = multiply([rightArm_1_conv, rightArm_2_conv])
+    unit1_multiply = multiply([unit1_left, unit1_right])
 
-    subtractedLayer = subtract([multiplyLayer_leftArm, multiplyLayer_rightArm])
+    # calculate unit2 inputs
+    unit2_left = reverse_layer(left_in(reverse_layer(model_input)))
+    unit2_right = reverse_layer(right_in(reverse_layer(model_input)))
 
-    size_x_sub = int(subtractedLayer.shape[2])
-    X = Conv2D(1, (1, size_x_sub), strides=(1, 1), name='conv2', kernel_initializer=glorot_uniform(seed=None))(
-        subtractedLayer)
+    unit2_multiply = multiply([unit2_left, unit2_right])
+
+    full_reich = subtract([unit1_multiply, unit2_multiply])
+
+    # combine all the correlators
+    # conv_x_size = int(x_layer2.shape[2])
+    conv_x_size = 1
+    combine_corr = Conv2D(1, (1, conv_x_size), strides=(1, 1), name='x_out',
+                   kernel_initializer=glorot_uniform(seed=None))(full_reich)
+
+    sum_reich = sum_layer(combine_corr)
 
     # Create model
-    model = Model(inputs=X_input, outputs=X, name='SimpleMotion')
+    model = Model(inputs=model_input, outputs=sum_reich, name='ReichCorr')
 
-    return model, pad, learning_rate
+    return model, pad_x, pad_t, learning_rate, batch_size
 
 
 def load_data_rr(path):
--- run_kerasNet.py
+++ run_kerasNet.py
@@ -6,14 +6,9 @@ from tensorflow.keras import optimizers
 from tensorflow.keras.utils import plot_model
 
 # define the input path
-# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_02s_10traces_40phi_100Hz_005devFrac.mat'
-# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_02s_10traces_40phi_100Hz_01devFrac.mat'
-# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_02s_10traces_40phi_100Hz_02devFrac.mat'
-# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_2s_6traces_355phi_100Hz_01devFrac.mat'
-
-# full images
-path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_2s_10traces_355phi_100Hz_005devFrac.mat'
-#path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_2s_10traces_355phi_100Hz_01devFrac.mat'
+path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_test.mat'
+# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_2s_10traces_355phi_100Hz_005devFrac.mat'
+# path = 'C:\\CDocuments\\python\\NaturalImages\\xtPlot_natImageCombinedFilteredContrast_2s_10traces_355phi_100Hz_01devFrac.mat'
 
 # load in data set
 x_train, y_train, x_dev, y_dev, x_test, y_test = md.load_data_rr(path)
@@ -33,21 +28,32 @@ plt.show()
 # intiialize model
 m, size_t, size_x, n_c = x_train.shape
 num_filt = 16
-# model, pad, learning_rate, batch_size = md.ln_model(input_shape=(size_t, size_x, n_c), filter_shape=(21, 5), num_filter=2)
-model, pad, learning_rate, batch_size = md.ln_model_deep(input_shape=(size_t, size_x, n_c), filter_shape=((21, 5), (21, 5)), num_filter=(4, 4))
-# model, pad, learning_rate = md.hrc_model(input_shape=(size_t, size_x, n_c), filter_shape=(21, size_x), num_filter=num_filter)
+# model, pad_x, pad_t, learning_rate, batch_size = md.ln_model(input_shape=(size_t, size_x, n_c), filter_shape=(21, 5), num_filter=2)
+# model, pad_x, pad_t, learning_rate, batch_size = md.ln_model_deep(input_shape=(size_t, size_x, n_c), filter_shape=((21, 20), (21, 10)), num_filter=(4, 4))
+model, pad_x, pad_t, learning_rate, batch_size = md.hrc_model(input_shape=(size_t, size_x, n_c), filter_shape=(21, 10), num_hrc=4)
+
+# repeat y data to fit output conv size
+# y_train = np.tile(y_train, (1, 1, size_x, 1))
+# y_dev = np.tile(y_dev, (1, 1, size_x, 1))
+# y_test = np.tile(y_test, (1, 1, size_x, 1))
 
 # get rid of edges of y data
-y_train = y_train[:, 0:-1-2*pad+1, :, :]
-y_dev = y_dev[:, 0:-1-2*pad+1, :, :]
-y_test = y_test[:, 0:-1-2*pad+1, :, :]
+# y_train = y_train[:, 0:-1-2*pad_t+1, pad_x:-1-pad_x+1, :]
+# y_dev = y_dev[:, 0:-1-2*pad_t+1, pad_x:-1-pad_x+1, :]
+# y_test = y_test[:, 0:-1-2*pad_t+1, pad_x:-1-pad_x+1, :]
+
+y_train = y_train[:, 0:-1-2*pad_t+1, :, :]
+y_dev = y_dev[:, 0:-1-2*pad_t+1, :, :]
+y_test = y_test[:, 0:-1-2*pad_t+1, :, :]
 
 # set up the model and fit it
 t = time.time()
 adamOpt = optimizers.Adam(lr=learning_rate)
 model.compile(optimizer=adamOpt, loss='mean_squared_error', metrics=[md.r2])
-hist = model.fit(x_train, y_train, verbose=2, epochs=10, batch_size=batch_size, validation_data=(x_dev, y_dev))
+hist = model.fit(x_train, y_train, verbose=2, epochs=20, batch_size=batch_size, validation_data=(x_dev, y_dev))
 elapsed = time.time() - t
+
+# grab the loss and R2 over time
 #model.save('kerasModel_' + str(num_filt) + 'Filt' + '.h5')
 loss = hist.history['loss']
 val_loss = hist.history['val_loss']
@@ -56,35 +62,33 @@ val_r2 = hist.history['val_r2']
 
 print('model took ' + str(elapsed) + 's to train')
 
-# get model predictions
-yHat_train = model.predict(x_train)
-yHat_dev = model.predict(x_dev)
+for l in model.layers:
+    all_weights = l.get_weights()
 
-# measure model accuracy
-train_accuracy = 1-np.sum(np.square(yHat_train - y_train)) / \
-    np.sum(np.square(y_train-np.mean(y_train)))
-dev_accuracy = 1 - np.sum(np.square(yHat_dev - y_dev)) / \
-    np.sum(np.square(y_dev - np.mean(y_dev)))
+    if len(all_weights) == 2:
+        weights = all_weights[0]
+        biases = all_weights[1]
 
-print('train accuracy: ' + str(train_accuracy))
-print('dev accuracy: ' + str(dev_accuracy))
+        images = weights+biases
 
-# plot weights
-W1 = model.layers[2].get_weights()
-W1 = W1[0]
+        maxAbsW1 = np.max(np.abs(images))
 
-maxAbsW1 = np.max(np.abs(W1))
+        for c in range(images.shape[2]):
+            plt.figure()
+            for w in range(images.shape[3]):
+                plt.subplot(2, int(np.ceil(images.shape[3]/2)), w+1)
+                img = plt.imshow(images[:, :, c, w])
+                plt.clim(-maxAbsW1, maxAbsW1)
+                plt.axis('off')
+                img.set_cmap('RdBu_r')
+                # plt.colorbar()
 
-plt.figure(1)
-for w in range(W1.shape[3]):
-    plt.subplot(2, int(np.ceil(W1.shape[3]/2)), w+1)
-    img = plt.imshow(W1[:, :, 0, w])
-    plt.clim(-maxAbsW1, maxAbsW1)
-    plt.axis('off')
-    img.set_cmap('RdBu_r')
-    # plt.colorbar()
+        plt.figure()
+        x = np.arange(len(biases))
+        plt.scatter(x, biases)
 
-plt.figure(2)
+# plot loss and R2
+plt.figure()
 plt.subplot(1, 2, 1)
 plt.plot(loss)
 plt.plot(val_loss)
