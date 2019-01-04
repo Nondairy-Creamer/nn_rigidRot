b7fdac496b5fe90f5180060bbf9504cb29e713b1
Matthew Creamer <matthews.s.creamer@gmail.com>
2019-01-03 23:01:03 -0700

    created matlab file for testing the fit models

--- NaturalImages/TestKerasModel.m
+++ NaturalImages/TestKerasModel.m
@@ -1,92 +1,297 @@
 function TestKerasModel
     %% load weights and images
-    w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsHrc.mat');
-    images = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\xtPlot_natImageCombinedFilteredContrast_20scenes_2s_10traces_355phi_100Hz_005devFrac.mat');
+%     images = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\xtPlot_natImageCombinedFilteredContrast_20scenes_2s_10traces_355phi_100Hz_005devFrac.mat');
+    images = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\xtPlot_natImageCombinedFilteredContrast_40scenes_1s_2traces_355phi_1000Hz_005devFrac.mat');
 
-    weightNames = fieldnames(w);
-    weightCell = cell(length(weightNames),1);
+
     
-    for wInd = 1:length(weightNames)
-        weightCell{wInd} = double(w.(weightNames{wInd}));
+    % boolean to decide whether to sum over space
+    sumOverSpace = false;
+    useOneTime = false;
+    %% choose filter type to use
+    filterType = 'gradientDescentSep';
+    
+    filtSizeIn = [21 11];
+    
+    b{1} = 0;
+    b{2} = 0;
+    b{3} = 0;
+    
+    switch filterType
+        case 'gradientDescentSep'
+%             w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsSep.mat');
+%             w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsSep_NoSum.mat');
+%             w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsSep_highRes_noSum.mat');
+            w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weights_highRes_longRun_sep.mat');
+            
+            weightNames = fieldnames(w);
+            weightCell = cell(length(weightNames),1);
+
+            for wInd = 1:length(weightNames)
+                weightCell{wInd} = double(w.(weightNames{wInd}));
+            end
+            
+            weightCell{5}(15:end) = 0; 
+            
+            h{1} = weightCell{1}*weightCell{3};
+            h{2} = weightCell{5}*weightCell{7};
+            h{3} = 1;
+            
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
+            h = cellfun(@(x)[x zeros(size(x,1),1)],h,'UniformOutput',false);
+            
+            h{3} = h{3}(1);
+            
+        case 'gradientDescent'
+            %     w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsHrc.mat');
+            w = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\weightsPretty.mat');
+
+            weightNames = fieldnames(w);
+            weightCell = cell(length(weightNames),1);
+
+            for wInd = 1:length(weightNames)
+                weightCell{wInd} = double(w.(weightNames{wInd}));
+            end
+            
+            h = weightCell(1:2:end);
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
+            h = cellfun(@(x)[x zeros(size(x,1),1)],h,'UniformOutput',false);
+            h{3} = h{3}(1);
+            h{2}(1,1) = 0;
+            
+            thresh = 0.11;
+            h{1}(abs(h{1})<thresh) = 0;
+            h{2}(abs(h{1})<thresh) = 0;
+            
+        case 'newRandom'
+            h{1} = randn(filtSizeIn)/10;
+            h{2} = randn(filtSizeIn)/10;
+            h{3} = 1;
+%             save('randomH','h');
+        case 'savedRandom'
+            hIn = load('C:\CDocuments\python\nn_RigidRot\NaturalImages\randomH');
+            h = hIn.h;
+            
+        case 'gaussian'
+            h{1} = zeros(filtSizeIn);
+            h{2} = zeros(filtSizeIn);
+            h{3} = 1;
+            
+        case 'deltas'
+            h{1} = zeros(filtSizeIn);
+            h{2} = zeros(filtSizeIn);
+            h{3} = 1;
+            
+            fillX1 = [1 1]';
+            fillY1 = [1 2]';
+            fillVal1 = [2 -1]';
+            
+            fillX2 = [(1:10)+1 (1:10)+1]';
+            fillY2 = [ones(1,10) 1+ones(1,10)]';
+            fillVal2 = [ones(1,10) -ones(1,10)]';
+            
+            for ff = 1:length(fillX1)
+                h{1}(fillY1(ff),fillX1(ff)) = fillVal1(ff);
+            end
+            
+            for ff = 1:length(fillX2)
+                h{2}(fillY2(ff),fillX2(ff)) = fillVal2(ff);
+            end
+            
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
+            
+        case 'deltas2'
+            h{1} = zeros(filtSizeIn);
+            h{2} = zeros(filtSizeIn);
+            h{3} = 1;
+            
+            fillX1 = [1:10 1:10]';
+            fillY1 = [ones(1,10) ones(1,10)+1]';
+            fillVal1 = [ones(1,10) -2*ones(1,10)]';
+            
+            fillX2 = [1 1]';
+            fillY2 = [1 2]';
+            fillVal2 = [1 -2]';
+            
+            for ff = 1:length(fillX1)
+                h{1}(fillY1(ff),fillX1(ff)) = fillVal1(ff);
+            end
+            
+            for ff = 1:length(fillX2)
+                h{2}(fillY2(ff),fillX2(ff)) = fillVal2(ff);
+            end
+            
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
+            
+        case 'deltas3'
+            h{1} = zeros(filtSizeIn);
+            h{2} = zeros(filtSizeIn);
+            h{3} = 1;
+            
+            fillX1 = [1 2 3 1 2 3 4:9 4:9 1]';
+            fillY1 = [1 1 1 2 2 2 1 1 1 1 1 1 2 2 2 2 2 2 3]';
+            fillVal1 = [-2 -1 -1 1 1 2 -.5 -.5 -.5 -.5 -.5 -.5 .5 .5 .5 .5 .5 .5 .5]';
+            
+            fillX2 = [2 2 5 6 5 6 1 1 3]';
+            fillY2 = [1 2 1 1 2 2 1 3 1]';
+            fillVal2 = [-1 2 -.5 -.5 .5 .5 -.5 -.5 -.5]';
+            
+            for ff = 1:length(fillX1)
+                h{1}(fillY1(ff),fillX1(ff)) = fillVal1(ff);
+            end
+            
+            for ff = 1:length(fillX2)
+                h{2}(fillY2(ff),fillX2(ff)) = fillVal2(ff);
+            end
+            
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
+            
+        case 'deltaSquare'
+            h{1} = zeros(filtSizeIn);
+            h{2} = zeros(filtSizeIn);
+            h{3} = 1;
+            
+%             fillX1 = (1:4)';
+%             fillY1 = (1:10)';
+%             
+%             fillX2 = fillX1+3;
+%             fillY2 = fillY1+10;
+
+            fillX1 = (1)';
+            fillY1 = (1)';
+            
+            fillX2 = fillX1+1;
+            fillY2 = fillY1+1;
+            
+            h{1}(fillY1,fillX1) = 1/10;
+            h{2}(fillY2,fillX2) = 1/10;
+            
+            h = cellfun(@(x)rot90(x,2),h,'UniformOutput',false);
     end
     
-    h = weightCell(1:2:end);
-    b = weightCell(2:2:end);
     
-    h{1} = randn(21,10)/10;
-    h{2} = randn(21,10)/10;
+    filtSize = size(h{1});
+    
+    padX = (filtSize(2)-1)/2;
+    padT = (filtSize(1)-1)/2;
     
     %% rearrange dimensions of python data
-    trainX = permute(images.trainX,[3 2 1]);
-    devX = permute(images.devX,[3 2 1]);
-    testX = permute(images.testX,[3 2 1]);
+    trainImage = permute(images.trainX,[3 2 1]);
+    devImage = permute(images.devX,[3 2 1]);
+    testImage = permute(images.testX,[3 2 1]);
 
-    trainY = permute(images.trainY,[2 1]);
-    devY = permute(images.devY,[2 1]);
-    testY = permute(images.testY,[2 1]);
+    trainVel = permute(images.trainY,[2 1]);
+    devVel = permute(images.devY,[2 1]);
+    testVel = permute(images.testY,[2 1]);
     
+    sizeImage = size(devImage);
+    sizeVelIn = size(devVel);
+    
+    %% truncate velocity vectors to account for valid convolutions
+    sizePred = [sizeVelIn(1) sizeVelIn(2)-2*padT sizeImage(3)-2*padX];
+    
+    devVelTrunc = repmat(devVel(1:sizePred(1),1:sizePred(2)),[1 1 sizePred(3)]);
     
     %% get predictions
-    m = size(devX,1);
-    pred = zeros(m,201);
+    m = size(devImage,1);
+    
+    pred = zeros(size(devVelTrunc));
     
     for mInd = 1:m
-        natScene = squeeze(devX(mInd,:,:));
+        natScene = squeeze(devImage(mInd,:,:));
         natScene_norm = natScene/std(natScene(:));
 %         pred(mInd,:) = LnModel(natScene_norm,h,b);
-        pred(mInd,:) = HrcModel(natScene_norm,h,b);
+        pred(mInd,:,:) = HrcModel(natScene_norm,h,b);
     end
     
-    devY_trim = devY(:,1:size(pred,2));
-    
-    %% calculate R2
-    R2 = zeros(m,1);
-    for mInd = 1:m
-        thisPred = pred(mInd,:)';
-        thisY = devY_trim(mInd,:)';
-        
-        R2(mInd) = 1 - sum((thisPred-thisY).^2)./sum((thisY-mean(thisY)).^2);
+    if sumOverSpace
+        pred = sum(pred,3);
+        devVelTrunc = devVelTrunc(:,:,1);
     end
     
-    MakeFigure;
-    plot(R2);
-    
-    globalY = devY_trim(:,1);
-    globalPred = pred(:,1);
-    
-    for rr = -100:100
-        globalR2(rr+101) = 1-sum((globalY-rr*globalPred).^2)./sum((globalY-mean(globalY)).^2);
+    if useOneTime
+        pred = pred(:,1,:);
+        devVelTrunc = devVelTrunc(:,1,:);
     end
-    max(globalR2)
     
-    MakeFigure;
-    scatter(globalY,globalPred);
-    
+    %% displace the steadystate response to sine waves
+    H = cell(length(h),1);
     for hInd = 1:length(h)
-        H{hInd} = fft2(h{hInd});
+        H{hInd} = fftshift(fft2(h{hInd}));
     end
     
     RH = real(H{1}.*conj(H{2})-fliplr(H{1}.*conj(H{2})));
     
-    imagesc(fftshift(RH));
+    %% calculate R2 across all examples and find the best R2 by scalling global pred
+    globalY = devVelTrunc(:);
+    globalPred = pred(:);
+    
+    mult = lsqlin(globalPred,globalY);
+    
+    bestR2 = 1-sum((globalY-mult*globalPred).^2)./sum((globalY-mean(globalY)).^2);
+
+    finalPred = pred*mult;
+    globalPred = finalPred(:);
+    
+    %% calculate R2 for each example
+    R2 = zeros(m,1);
+    for mInd = 1:m
+        thisPred = finalPred(mInd,:,:);
+
+        thisVel = devVelTrunc(mInd,:,:);
+        
+        thisPred = thisPred(:);
+        thisVel = thisVel(:);
+        
+        R2(mInd) = 1 - sum((thisPred-thisVel).^2)./sum((thisVel-mean(thisVel)).^2);
+    end
+    
+    %% plot figs
+    disp(bestR2);
+        
+    MakeFigure;
+    max_abs_h = max(abs([h{1}(:); h{2}(:)]));
+    subplot(2,2,1);
+    imagesc(h{1});
+    caxis([-max_abs_h max_abs_h]);
+    
+    subplot(2,2,2);
+    imagesc(h{2})
+    caxis([-max_abs_h max_abs_h]);
+
+    subplot(2,2,3);
+    imagesc(RH);
+    
+    colormap(flipud(cbrewer('div','RdBu',100)));
+    
+    MakeFigure;
+    plot(R2);
+    xlabel('example');
+    ylabel('r2');
+    
+    MakeFigure;
+    scatter(globalY,globalPred);
+    xlabel('scene velocity');
+    ylabel('model prediction');
+    
 end
 
 function pred = LnModel(img,h,b)
-    arm1 = imfilter(img,h{1},'same')+b{1};
+    arm1 = conv2(img,h{1},'valid')+b{1};
     arm1(arm1<0) = 0;
     
-    arm2 = imfilter(img,fliplr(h{1}),'same')+b{1};
+    arm2 = imfilter(img,fliplr(h{1}),'valid')+b{1};
     arm2(arm2<0) = 0;
     
-    pred = sum((arm1-arm2)*h{2}+b{2},2);
+    pred = (arm1-arm2)*h{2}+b{2};
 end
 
 function pred = HrcModel(img,h,b)
-    arm1 = imfilter(img,h{1},'same')+b{1};
-    arm2 = imfilter(img,h{2},'same')+b{2};
+    arm1 = conv2(img,h{1},'valid')+b{1};
+    arm2 = conv2(img,h{2},'valid')+b{2};
     
-    arm3 = imfilter(img,fliplr(h{1}),'same')+b{1};
-    arm4 = imfilter(img,fliplr(h{2}),'same')+b{2};
+    arm3 = conv2(img,fliplr(h{1}),'valid')+b{1};
+    arm4 = conv2(img,fliplr(h{2}),'valid')+b{2};
     
-    pred = sum((arm1.*arm2-arm3.*arm4)*h{3}+b{3},2);
+    pred = (arm1.*arm2-arm3.*arm4)*h{3}+b{3};
 end
